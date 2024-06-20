7ae38a84af23254a4a8f1b621ceee9714b812ad9
akki5993 <akshaypatel5991@gmail.com>
2024-06-20 08:15:04 +0530

    add static files & views folder

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 const userModel = require("./connection/conn");
+const ejs = require("ejs");
+const path = require("path");
+//const cookieParser = require("cookie-parser");
+//const session = require("express-session");
+
+//app.use(cookieParser());
+// app.use(
+//   session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: "akshaypatel",
+//   })
+// );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("WElcome to Todo App");
+  res.render("index");
 });
 
 app.get("/register", async (req, res) => {
@@ -20,5 +37,24 @@ app.get("/allusers", async (req, res) => {
   const allusers = await userModel.find();
   res.send(allusers);
 });
+
+// app.get("/sessionset", (req, res) => {
+//   req.session.name = "Akshay";
+//   res.send("Hello");
+// });
+
+// app.get("/sessionget", (req, res) => {
+//   console.log(req.session);
+// });
+
+// app.get("/cookieset", (req, res) => {
+//   res.cookie("name", "akshay");
+//   res.send("Set Cookies");
+// });
+
+// app.get("/cookieget", (req, res) => {
+//   console.log(req.cookies);
+//   res.send(req.cookies);
+//});
 
 app.listen(3000);
