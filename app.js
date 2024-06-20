354f2be628a2121822a3a97ce125383e32efbdd2
akki5993 <akshaypatel5991@gmail.com>
2024-06-20 12:37:16 +0530

    add bcrypt,jsonwebtoken functionalities

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
-const userModel = require("./connection/conn");
+const userModel = require("./connection/usermodel");
 const ejs = require("ejs");
 const path = require("path");
-//const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 //const session = require("express-session");
 
-//app.use(cookieParser());
+app.use(cookieParser());
 // app.use(
 //   session({
 //     resave: false,
@@ -23,20 +25,62 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/register", async (req, res) => {
-  let { email, password } = req.query;
+app.post("/create", async (req, res) => {
+  let { email, password } = req.body;
 
-  const userCreated = await userModel.create({
-    email: email,
-    password: password,
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(password, salt, async function (err, hash) {
+      const userCreated = await userModel.create({
+        email: email,
+        password: hash,
+      });
+
+      let token = jwt.sign({ email }, "privatekey");
+      res.cookie("token", token);
+      res.redirect("/tasks");
+    });
   });
-  res.send(userCreated);
 });
 
-app.get("/allusers", async (req, res) => {
-  const allusers = await userModel.find();
-  res.send(allusers);
+app.get("/login", (req, res) => {
+  res.render("login");
 });
+
+app.post("/login", async (req, res) => {
+  let user = await userModel.findOne({ email: req.body.email });
+  if (!user) return res.redirect("/login");
+
+  bcrypt.compare(req.body.password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign({ email: user.email }, "privatekey");
+      res.cookie("token", token);
+      res.redirect("tasks");
+    }
+  });
+});
+
+app.get("/tasks", (req, res) => {
+  res.render("tasks");
+});
+
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/");
+});
+// app.get("/register", async (req, res) => {
+//   let { email, password } = req.query;
+
+//   const userCreated = await userModel.create({
+//     email: email,
+//     password: password,
+//   });
+//   res.send(userCreated);
+// });
+
+// app.get("/allusers", async (req, res) => {
+//   const allusers = await userModel.find();
+//   res.send(allusers);
+// });
 
 // app.get("/sessionset", (req, res) => {
 //   req.session.name = "Akshay";
